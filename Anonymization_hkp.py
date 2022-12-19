a6f8efd407781cdf50143bcca7e26b6c9693e8eb
chiara-cossu <chiara.cossu00@gmail.com>
2022-12-19 13:04:12 +0100

    da testare parte 2, manca parte 3

--- Anonymization_hkp.py
+++ Anonymization_hkp.py
@@ -17,27 +17,33 @@ class Anonymization_hkp:
     number of 1 in column (beta=1)
     """
     def sup(self,beta):
-        print(beta)
         if len(beta) == 1:
-            return self.df[beta].sum()
+            #print("beta ", beta, "sup ", self.df[beta].sum().values.item())
+            return self.df[beta].sum().values.item()  # (senza .values dava una serie)
         sum_ = self.df[beta].sum(axis=1)  # sum_ is a list: sum all columns to check how many rows are equal to 111...
-        return sum_.value_counts()[len(beta)].item()
+        #print("sum_", sum_)
+        s = sum_.value_counts()
+        if len(beta) not in s:
+            return 0
+        return s[len(beta)].item()
     
 
     def p_breach(self, beta):  # TODO: use sup() 
-        occurr = []  # for each private item the number of occurrencies
+        prob = []  # for each private/sensitive item the probability in beta (# occurr/sup(temp)
         for e in self.sensitive:
             tmp = beta.copy()
             tmp.append(e)
             sum_ = self.df[tmp].sum(axis=1)  # sum_ is a list: sum all columns to check how many rows are equal
             s = sum_.value_counts()
             if (len(tmp)) not in s:
+                prob.append(0)
                 continue
             else:
-                occurr.append(sum_.value_counts()[len(tmp)])  # save the result
-            
-        print(occurr)
-        return np.max(occurr) / self.sup(tmp)  # take the max result and calculate probability
+                occurr = sum_.value_counts()[len(tmp)]
+                #print("occurr ", occurr, " sup ", self.sup(beta))
+                prob.append(occurr / self.sup(beta))  # save the result
+                #print("prob ", occurr / self.sup(beta))
+        return np.max(prob)  # take the max probability
                 
 
 
@@ -48,11 +54,12 @@ class Anonymization_hkp:
     """
     # step 1) preprocessing: eliminate all size 1 moles
     def suppress_size1_mole(self):
-        for cmole in self.public:   # candidate mole
+        public_copy = self.public.copy()  # copy for iteration
+        for cmole in public_copy:   # candidate mole
             s = self.sup([cmole])
             p_br = self.p_breach([cmole])
-            if s.item() < self.k or p_br > self.h:  # check if cmole is a mole
-                print("----------------------")
+            if s < self.k or p_br > self.h:  # check if cmole is a mole
+                #print("----------------------")
                 self.df.drop(inplace=True,columns=cmole,axis=1)  # eliminate the mole
                 self.public.remove(cmole)
 
@@ -61,19 +68,21 @@ class Anonymization_hkp:
     def find_minimal_moles(self):
         all_M = []
         i = 1
-        f = self.df.columns  # after preprocessing we have f = F1 (set)
-        while i < self.p and f:  # f not empty
-            temp = set.union(*f)  # union of groups of dimension i and then make new groups of dimension i+1
-            c = set(combinations(temp, n)) # candidate set C_(i+1)
+        f = set(self.public)  # after preprocessing we have f = F1 (set)
+        while i < self.l and f:  # f not empty
+            temp = set.union(f)  # union of groups of dimension i and then make new groups of dimension i+1
+            c = set(combinations(temp, i+1)) # candidate set C_(i+1)
             temp_M = []
             temp_F = []
             for beta in c:
-                if self.sup(beta) < self.k or self.p_breach(beta):  # beta is a mole
-                    temp_M.append(beta)
+                #print("i ", i, ": ", self.sup(list(beta)))
+                #print("len di beta step 2", len(beta))
+                if self.sup(list(beta)) < self.k or self.p_breach(list(beta)) > self.h:  # beta is a mole
+                    temp_M.append(beta)  # beta is a tuple: you can do set of tuples but can not do set of lists because list is not hashable
                 else:
                     temp_F.append(beta)  # beta is not a mole
             all_M.append(temp_M)
-            f = temp_F  # substitute F_i with F_(i+1)
+            f = set(temp_F)  # substitute F_i with F_(i+1)
             i += 1
         return all_M
 
--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import pandas as pd
 import Anonymization_hkp as hkp
 
 # sensitive = [0,5,9,15,30,47]
-sensitive = [2,3]
+sensitive = [7,8,9]
 h = 0.3
-k = 1
-p = 3
+k = 4
+p = 4
 
-l = 4
+l = 5
 
 if __name__ == "__main__":
     # import dataset
-    #filename = "datasets/dataBMS1_transaction.csv"
-    filename = "datasets/test.csv"
+    filename = "datasets/dataBMS1_transaction.csv"
+    #filename = "datasets/test.csv"
     df = pd.read_csv(filename)
+    val1 = [i for i in range(20,df.shape[1])]
+    df.drop(df.columns[val1],inplace=True,axis=1)
     
     # add indexes
     df.columns = [ i for i in range(len(df.columns)) ] 
 
-    print(df)
+    #print(df)
     anon = hkp.Anonymization_hkp(df,sensitive,h,k,p,l)
     # preprocessing
     anon.suppress_size1_mole()
+    print("preprocessing: ended")
+    Mms = anon.find_minimal_moles()
+    print(Mms)
     print(anon.df)
